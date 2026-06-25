e7569b2278b3755515831ccba6cf11dcc8cc0658
atexombrax-dot <atexombrax@gmail.com>
2026-06-25 01:11:41 +0100

    Add Pi Network login function and event listener

---
+++ Frontend/piAuth.js
@@ -0,0 +1,10 @@
+async function loginWithPi() {
+console.log("Pi Network authentication");
+
+// Pi SDK integration will be added here
+
+}
+
+document
+.getElementById("pi-login-btn")
+?.addEventListener("click", loginWithPi);
